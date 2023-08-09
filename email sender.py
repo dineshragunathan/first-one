65bdb92848ffe19e4840a10abd5e9a575c5f8c59
Dinesh Ragunathan <141299112+dineshragunathan@users.noreply.github.com>
2023-08-09 10:47:42 +0530

    pj-2.py (email sender)

---
+++ email sender.py
@@ -0,0 +1,23 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'mydrive2005688@gmail.com'
+email_reciever = 'toxodew821@royalka.com'
+email_pass = 'xlesjokhcqqemapy'
+subject = 'trying out email sender . PY'
+body = ''' 
+Just trying out new stuff nvm!!
+'''
+em = EmailMessage()
+em['From']=email_sender
+em['To']=email_reciever
+em['Subject']=subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+    smtp.login(email_sender,email_pass)
+    smtp.sendmail(email_sender,email_reciever,em.as_string())
+
